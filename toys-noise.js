355e8bd795c6bb91a033eb6b5eaa60a73d101516
rjbultitude <richard.bultitude@gmail.com>
2019-12-23 21:04:42 +0000

    fix broken paths. Fine tune sketch settings

--- toys-noise.js
+++ toys-noise.js
@@ -9,69 +9,68 @@
  * $notes        Notes
  */
 
-define(['p5'], function(p5) {
-    'use strict';
 
-    var thisSketch;
+'use strict';
 
-    var myp5 = new p5(function(sketch) {
-        thisSketch = sketch;
-        mySketch();
-    },'toys-noise');
+var thisSketch;
 
-    function mySketch() {
-        thisSketch.setup = setup;
-        thisSketch.draw = draw;
+var myp5 = new p5(function(sketch) {
+    thisSketch = sketch;
+    mySketch();
+},'toys-noise');
 
-        //vars
-        var xstart;
-        var xnoise;
-        var ynoise;
-        var ystart;
-        var spacing = 5;
-        var amt = 0.05;
+function mySketch() {
+    thisSketch.setup = setup;
+    thisSketch.draw = draw;
 
-        function setup() {
-            thisSketch.frameRate(30);
-            var myCanvas = thisSketch.createCanvas(600, 300);
-            myCanvas.parent('toys-noise');
+    //vars
+    var xstart;
+    var xnoise;
+    var ynoise;
+    var ystart;
+    var spacing = 3;
+    var amt = 0.05;
+    var speed = 0.05;
 
-            xstart = thisSketch.random(10);
-            ystart = thisSketch.random(10);
-            
-        }
+    function setup() {
+        thisSketch.frameRate(60);
+        var myCanvas = thisSketch.createCanvas(500, 100);
+        myCanvas.parent('toys-noise');
 
-        function draw() {
-            thisSketch.background(0, 0, 0);
+        xstart = thisSketch.random(10);
+        ystart = thisSketch.random(10);
+        
+    }
 
-            xstart += 0.01;
-            ystart += 0.01;
+    function draw() {
+        thisSketch.background(0, 0, 0);
 
+        xstart += speed;
+        ystart += speed;
+
+        xnoise = xstart;
+        ynoise = ystart;
+
+        //loop through rows & columns
+        for (var y = 0; y <= thisSketch.height; y+=spacing) {
+            //increment y noise
+            ynoise += amt;
+            //reset x noise
             xnoise = xstart;
-            ynoise = ystart;
-
-            //loop through rows & columns
-            for (var y = 0; y <= thisSketch.height; y+=spacing) {
-                //increment y noise
-                ynoise += amt;
-                //reset x noise
-                xnoise = xstart;
-                for (var x = 0; x <= thisSketch.width; x+=spacing) {
+            for (var x = 0; x <= thisSketch.width; x+=spacing) {
                 xnoise += amt;
                 //call function with new values
                 drawPoint(x, y, thisSketch.noise(xnoise, ynoise));
-                }
             }
         }
-
-        function drawPoint(x, y, noiseFactor) {
-            thisSketch.push();
-            thisSketch.translate(x, y);
-            //rotate(noiseFactor * radians(360));
-            thisSketch.noStroke();
-            thisSketch.fill(100, noiseFactor * 255, 220, 255);
-            thisSketch.rect(0, 0, noiseFactor * thisSketch.radians(360), noiseFactor * thisSketch.radians(360));
-            thisSketch.pop();
-        }
     }
-});
+
+    function drawPoint(x, y, noiseFactor) {
+        thisSketch.push();
+        thisSketch.translate(x, y);
+        thisSketch.noStroke();
+        thisSketch.fill(100, noiseFactor * 255, 220, noiseFactor * 255);
+        thisSketch.rect(0, 0, noiseFactor * thisSketch.radians(360), noiseFactor * thisSketch.radians(360));
+        thisSketch.pop();
+    }
+}
